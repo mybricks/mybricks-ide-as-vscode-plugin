d41c7e4e9b82bc38a4060fa776b4f9277ddd911d
xujk <936333046@qq.com>
2025-12-31 15:44:21 +0800

    样式

--- asserts/app.tsx
+++ asserts/app.tsx
@@ -19,7 +19,25 @@ const { SPADesigner } = (window as any).mybricks
 // 获取配置函数（由 config.tsx 挂载）
 const configFn = (window as any).config
 
-const { Popover, Form, Input, Button, message } = (window as any).antd
+const ANTD_CONFIG = {
+  theme: {
+    token: {
+      colorPrimary: '#fa6400',
+    },
+  },
+}
+const {
+  ConfigProvider,
+  Popover,
+  Form,
+  Input,
+  Button,
+  message,
+  Space,
+  Tooltip,
+  Flex,
+  Alert,
+} = (window as any).antd
 const { VerticalAlignBottomOutlined } = (window as any).icons
 
 const vsCodeMessage = (window as any).webViewMessageApi
@@ -88,45 +106,49 @@ function App() {
   }, [])
 
   return (
-    <div className='ide'>
-      {/* 顶部工具栏 */}
-      <div className='toolbar'>
-        <div className={'logo'}>MyBricks</div>
-        <div className={'btns'} id={'toolbarBtns'} />
-        <div>
-          <button className={'primary'} onClick={save}>
-            {changed ? '*' : ''}保存
-          </button>
-          <Popover
-            title='导出应用的源代码'
-            open={exportPopoverVisible}
-            onOpenChange={setExportPopoverVisible}
-            trigger='click'
-            placement='bottomRight'
-            arrow={false}
-            content={
-              <ExportContent
-                onExport={handleExport}
-                onClose={() => setExportPopoverVisible(false)}
-              />
-            }
-          >
-            <VerticalAlignBottomOutlined />
-          </Popover>
+    <ConfigProvider {...ANTD_CONFIG}>
+      <div className='ide'>
+        {/* 顶部工具栏 */}
+        <div className='toolbar'>
+          <div className={'logo'}>MyBricks</div>
+          <div className={'btns'} id={'toolbarBtns'} />
+          <Space>
+            <button className={'button primary'} onClick={save}>
+              {changed ? '*' : ''}保存
+            </button>
+            <Popover
+              title='导出应用的源代码'
+              open={exportPopoverVisible}
+              onOpenChange={setExportPopoverVisible}
+              trigger='click'
+              placement='bottomRight'
+              arrow={false}
+              content={
+                <ExportContent
+                  onExport={handleExport}
+                  onClose={() => setExportPopoverVisible(false)}
+                />
+              }
+            >
+              <Tooltip title={'导出应用的源代码'}>
+                <Button type='text' icon={<VerticalAlignBottomOutlined />} />
+              </Tooltip>
+            </Popover>
+          </Space>
+        </div>
+
+        {/* 设计器主区域 */}
+        <div className={'designer'}>
+          <SPADesigner
+            config={config}
+            ref={designerRef}
+            onLoad={(e) => console.log('loaded')}
+            onMessage={onMessage}
+            onEdit={() => setChanged(changed + 1)}
+          />
         </div>
       </div>
-
-      {/* 设计器主区域 */}
-      <div className={'designer'}>
-        <SPADesigner
-          config={config}
-          ref={designerRef}
-          onLoad={(e) => console.log('loaded')}
-          onMessage={onMessage}
-          onEdit={() => setChanged(changed + 1)}
-        />
-      </div>
-    </div>
+    </ConfigProvider>
   )
 }
 
@@ -179,81 +201,100 @@ function ExportContent(Iprops: {
   )
 
   return (
-    <div>
-      <Form
-        form={form}
-        layout='vertical'
-        size='small'
-        initialValues={defaultValues}
+    <Form
+      className={'export-form'}
+      form={form}
+      layout='vertical'
+      size='small'
+      initialValues={defaultValues}
+    >
+      <Form.Item
+        label='应用名称'
+        name='projectName'
+        rules={[
+          {
+            validator: (_, value: string) =>
+              /^[a-zA-Z][a-zA-Z0-9_]*$/.test(value)
+                ? Promise.resolve()
+                : Promise.reject(
+                    new Error('以字母开头，仅支持字母、数字以及下划线')
+                  ),
+          },
+        ]}
       >
-        <Form.Item
-          label='应用名称'
-          name='projectName'
-          rules={[
-            {
-              validator: (_, value: string) =>
-                /^[a-zA-Z][a-zA-Z0-9_]*$/.test(value)
-                  ? Promise.resolve()
-                  : Promise.reject(
-                      new Error('以字母开头，仅支持字母、数字以及下划线')
-                    ),
-            },
-          ]}
-        >
-          <Input
-            placeholder='请输入应用名称'
-            onBlur={() => {
-              form
-                .validateFields(['projectName'])
-                .then((values: ExportConfig) => {
-                  save('projectName', values.projectName)
-                })
-            }}
-          />
-        </Form.Item>
-        <Form.Item label='应用导出目录' name='exportDir' shouldUpdate>
-          {exportDir ? (
-            <div>
-              <span title={exportDir} style={{ marginRight: 8 }}>
-                {exportDir.split(/[\\/]/).pop()}
-              </span>
-              <Button onClick={onClearDir}>重置</Button>
-            </div>
-          ) : (
-            <Button type='primary' onClick={onSelectDir}>
-              配置目录
-            </Button>
-          )}
-        </Form.Item>
+        <Input
+          placeholder='请输入应用名称'
+          onBlur={() => {
+            form
+              .validateFields(['projectName'])
+              .then((values: ExportConfig) => {
+                save('projectName', values.projectName)
+              })
+          }}
+        />
+      </Form.Item>
+      <Form.Item label='应用导出目录' name='exportDir' shouldUpdate>
+        {exportDir ? (
+          <div>
+            <span title={exportDir} style={{ marginRight: 8 }}>
+              {exportDir.split(/[\\/]/).pop()}
+            </span>
+            <Button onClick={onClearDir}>重置</Button>
+          </div>
+        ) : (
+          <Button type='primary' onClick={onSelectDir}>
+            配置目录
+          </Button>
+        )}
+      </Form.Item>
 
-        <Form.Item shouldUpdate>
-          {() => (
-            <div>
-              <Button onClick={onClose}>取消</Button>
-              <Button
-                type='primary'
-                disabled={
-                  form.getFieldsError().filter(({ errors }) => errors.length)
-                    .length
-                }
-                onClick={() => handleExport(false)}
-              >
-                同步应用
-              </Button>
-              <Button
-                type='primary'
-                disabled={
-                  form.getFieldsError().filter(({ errors }) => errors.length)
-                    .length
-                }
-                onClick={() => handleExport(true)}
-              >
-                导出应用
-              </Button>
-            </div>
-          )}
-        </Form.Item>
-      </Form>
-    </div>
+      <Form.Item>
+        <Alert
+          message={
+            <Space direction='vertical' size='small'>
+              <h3 className={'export-alert-txt'}>注意事项</h3>
+              <p className={'export-alert-txt'}>导出当前应用的Taro源代码；</p>
+              <p className={'export-alert-txt'}>
+                【同步应用】以写文件覆盖的形式直接同步至目标目录下
+              </p>
+              <p className={'export-alert-txt'}>
+                【导出应用】打包成zip文件导出至目标目录下
+              </p>
+            </Space>
+          }
+          type='warning'
+        />
+      </Form.Item>
+
+      <Form.Item shouldUpdate style={{ marginBottom: 0 }}>
+        {() => (
+          <Flex gap='small' justify='flex-end'>
+            <Button onClick={onClose}>取消</Button>
+            <Button
+              type='primary'
+              disabled={
+                !exportDir ||
+                form.getFieldsError().filter(({ errors }) => errors.length)
+                  .length
+              }
+              onClick={() => handleExport(true)}
+            >
+              导出应用
+            </Button>
+            <Button
+              type='primary'
+              disabled={
+                !exportDir ||
+                form.getFieldsError().filter(({ errors }) => errors.length)
+                  .length
+              }
+              onClick={() => handleExport(false)}
+            >
+              同步应用
+            </Button>
+          </Flex>
+        )}
+      </Form.Item>
+    </Form>
   )
 }
